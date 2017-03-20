1228ad591e1407223f9ea59b879cbd372532054d
USER <USER@LG>
2017-03-20 16:47:48 +0900

    2017.3.20  계좌 만듬

--- Solution/src/Account.java
+++ Solution/src/Account.java
@@ -7,6 +7,10 @@ public class Account
 	
 	private double money;
 	
+	public void balance() {		
+		System.out.println(this.money);
+	}
+		
 	public Account() {					
 	}
 	public Account(double money) {		
@@ -24,27 +28,8 @@ public class Account
 			money -= minus;
 		}
 	}
-	/*
-	public void debit() {				//(�씤異�)
-		//System.out.println("");		//(�겢�옒�뒪 �씠由� �꽆湲곕뒗 諛⑸쾿?)
-		
-		Scanner sc = new Scanner(System.in);
-		double minus = sc.nextDouble();
-		
-		//(if臾� 援ы쁽 �븘�슂) -- (�븘�땲硫� Exception handling)
-		if (money < minus) {
-			System.out.println("Debit amount exceeded account balance.");
-		}
-		else {
-			money -= minus;
-		}
-	}
-	*/
-	
-	public void balance() {		
-		System.out.println(this.money);
-	}
 	
 	
+
 	
 }
--- Solution/src/Accounttest.java
+++ Solution/src/Accounttest.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-// (�떎�뻾 �겢�옒�뒪)
 
 
 public class Accounttest {
@@ -12,20 +11,14 @@ public class Accounttest {
 
 		Account account1 = new Account(50.00);
 		Account account2 = new Account();
-		/*
-		Account account1 = new Account();
-		Account account2 = new Account();
-		account1.credit(50.00);
-		// account2.credit(0.00);
-		*/
-		
+	
 		System.out.print("account1 balance: ");
 		account1.balance();
 		System.out.print("account2 balance: ");
 		account2.balance();
 		
 		
-//		account1.debit(25.67);
+
 		System.out.print("Enter withdrawl amount for account1: ");
 		Scanner sc = new Scanner(System.in);
 		input = sc.nextDouble();
@@ -37,7 +30,7 @@ public class Accounttest {
 		account2.balance();
 		
 		
-//		account2.debit(10.00);
+
 		System.out.print("Enter withdrawl amount for account2: ");
 		sc = new Scanner(System.in);		//(�씠誘� sc 媛앹껜瑜� 留뚮뱾�뿀�쑝�땲 �꽑�뼵 �깮�왂)
 		input = sc.nextDouble();
